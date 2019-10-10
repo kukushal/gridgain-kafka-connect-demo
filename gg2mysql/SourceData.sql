d37e0eec23b7bff197087308e0f87ace05760057
kukushal <akukushkin@gridgain.com>
2019-10-10 13:10:22 +0300

    SQL script added

--- gg2mysql/SourceData.sql
+++ gg2mysql/SourceData.sql
@@ -1,7 +1,13 @@
+-- Initial data
 CREATE TABLE IF NOT EXISTS Person (id int, city_id int, name varchar, PRIMARY KEY (id));
+
 INSERT INTO Person (id, name, city_id) VALUES (1, 'John Doe', 3);
 INSERT INTO Person (id, name, city_id) VALUES (2, 'John Smith', 4);
 
+
+
+-- Realtime data
 CREATE TABLE IF NOT EXISTS City (id int, name varchar, PRIMARY KEY (id));
+
 INSERT INTO City (id, name) VALUES (3, 'San-Francisco');
 INSERT INTO City (id, name) VALUES (4, 'Oakland');
